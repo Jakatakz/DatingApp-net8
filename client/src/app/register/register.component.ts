5dfb7d09be6ab0b9826e90c721729256d57e08bd
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-03-19 02:21:30 -0400

    input decorator for register and home, home parent

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent
 {
+  @Input() usersFromHomeComponent: any;
   model: any = {}
 
   register()
