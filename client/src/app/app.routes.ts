3ed9a48089d819ef4de527223226baaef0136a1f
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-03-24 05:47:46 -0400

    added some routes and new components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'members', component: MemberListComponent},
+    {path:'members/:id', component: MemberDetailComponent},
+    {path:'', component: HomeComponent},
+    {path:'', component: HomeComponent},
+    {path:'', component: HomeComponent}
+];
