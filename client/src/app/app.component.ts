14be7a182b118f5fdc3b52d158d0a63a74d5de15
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-03-31 09:20:11 -0400

    had to add title for some test for some reason

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
+  title = 'client';
 
   ngOnInit(): void {
       this.setCurrentUser();
