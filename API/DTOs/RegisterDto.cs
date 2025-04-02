0054b729b8c98066e21148fc0356e752bb0a76d1
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-04-02 00:43:33 -0400

    testing errors in the client

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,6 @@ public class RegisterDto
     // intsead of using 'required' below, you can use an attribute above the line by doing [Required]
     [Required]
     [MaxLength(100)]
-    public required string Username { get; set; }
-    public required string Password { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,7 @@ if (app.Environment.IsDevelopment())
 
 //app.UseAuthorization();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
