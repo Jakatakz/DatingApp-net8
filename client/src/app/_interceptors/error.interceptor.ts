9f311743bed3c1de26fd2c590af7a1873d30182e
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-04-03 02:44:31 -0400

    unfinihsed intercepts errors, 7:35

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,37 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const router = inject(Router);
+  const toastr = inject(ToastrService);
+
+  return next(req).pipe(
+    catchError(error => {
+      if (error) {
+        switch (error.status) {
+          case 400:
+            if (error.error.errors) {
+              const modalStateErrors = []; // e rrors and validation errors are referred to as model state errors in our api
+              for (const key in error.error.errors) {
+                if (error.error.errors[key]) {
+                  modalStateErrors.push(error.error.errors[key])
+                }
+              }
+              throw modalStateErrors.flat();
+            } else {
+              toastr.error(error.error, error.status)
+            }
+            break;
+            case 401:
+        
+          default:
+            break;
+        }
+      }
+    })
+  ); //observable, an httpevent of an unknown type.
+};
