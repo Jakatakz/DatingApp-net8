56e8266c5e4a839a57136d7be92fea26229cb5e9
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-04-10 02:36:36 -0400

    api dto registerdto?

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,10 @@ public class RegisterDto
     [Required]
     [MaxLength(100)]
     public string Username { get; set; }
+
+
+    [Required]
+    [MaxLength(100)]
     [StringLength(8, MinimumLength = 4)]
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 }
