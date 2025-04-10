70f98e05c3dc68a40aac7e08c8cfc57d9f4e6e95
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-04-10 02:30:28 -0400

    start of 8 in 7

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,9 +7,9 @@ namespace API.Data;
 public class DataContext : DbContext
 {
 
-    public DbSet<AppUser> Users { get; set; }
     public DataContext(DbContextOptions options) : base(options)
     {
         
     }
+    public DbSet<AppUser> Users { get; set; }
 }
