8c1efe0ef95d3f080d62b1463d4049e6e9cdddd1
Jax <12698100+Jakatakz@users.noreply.github.com>
2025-04-18 02:42:36 -0400

    dtos, repository for user db, modifying controllers

---
+++ API/Helpers/AutoMappersProfiles.cs
@@ -0,0 +1,15 @@
+﻿using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMappersProfiles : Profile
+    {
+        public AutoMappersProfiles()
+        {
+            CreateMap<AppUser, MemberDto>();
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
